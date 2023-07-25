6fe59b96a9b5e1161af0ddea96643beec1730a73
Hopp-Murithi <hoppmurithi@gmail.com>
2023-07-25 20:07:36 +0300

    feat: implememnt modular file structure

--- public/app.js
+++ public/app.js
@@ -1,10 +1,22 @@
-"use strict";
+import { Invoice } from './classes/invoice.js';
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const amount = document.querySelector("#amount");
 const details = document.querySelector("#details");
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault(); //prevents page from refreshing when we submit a form
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
+//instances of the invoice
+const invOne = new Invoice("Murithi", "website work", 2500);
+const invTwo = new Invoice("Hope", "System design work", 250000);
+console.log(invOne, invTwo);
+let invoices = []; //instantiate invoices to take an array. if you pass string or number it will throw an error
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+invoices.map((inv) => {
+    console.log(inv.format());
+});
+console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import {Invoice} from './classes/invoice.js'
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 const type = document.querySelector("#type") as HTMLSelectElement;
@@ -5,13 +7,27 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 
-
-form.addEventListener('submit', (e:Event)=>{
+form.addEventListener("submit", (e: Event) => {
   e.preventDefault(); //prevents page from refreshing when we submit a form
-  console.log(
-    type.value,
-    tofrom.value,
-    details.value,
-    amount.valueAsNumber
-  )
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
+
+
+//instances of the invoice
+const invOne = new Invoice("Murithi", "website work", 2500);
+const invTwo = new Invoice("Hope", "System design work", 250000);
+
+console.log(invOne, invTwo);
+
+let invoices: Invoice[] = []; //instantiate invoices to take an array. if you pass string or number it will throw an error
+
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
+
+invoices.map((inv) => {
+  console.log(inv.format());
+});
+
+console.log(invoices);
